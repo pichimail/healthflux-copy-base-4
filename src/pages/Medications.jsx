a9cf2cd0ea2937e491fed1368f6286adafded476
pichi mail <pichimail24@gmail.com>
2025-12-09 21:49:59 +0000

    File changes

--- src/pages/Medications.jsx
+++ src/pages/Medications.jsx
@@ -227,8 +227,7 @@ export default function Medications() {
         </TabsList>
 
         <TabsContent value="medications">
-          {/* Medications List */}
-      {isLoading ? (
+          {isLoading ? (
         <div className="flex justify-center py-12">
           <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-purple-600" />
         </div>
