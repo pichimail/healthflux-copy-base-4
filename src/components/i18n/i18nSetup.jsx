bc80c5281318648615e61ce5d242583111738a6d
pichi mail <pichimail24@gmail.com>
2025-12-11 10:03:57 +0000

    File changes

--- src/components/i18n/i18nSetup.jsx
+++ src/components/i18n/i18nSetup.jsx
@@ -1,3 +1,4 @@
+
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -20,7 +21,11 @@ i18n
     },
     detection: {
       order: ['localStorage', 'navigator'],
-      caches: ['localStorage']
+      caches: ['localStorage'],
+      lookupLocalStorage: 'userLanguage'
+    },
+    react: {
+      useSuspense: false
     }
   });
 
